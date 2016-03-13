b1fc2fbf96e9bb39439bc1033ead19d3ad096654
os_lab_user <you@example.com>
2016-03-13 19:43:30 +0800

    lab1 ex1-6

--- ucore_os_lab/labcodes/lab1/kern/trap/trap.c
+++ ucore_os_lab/labcodes/lab1/kern/trap/trap.c
@@ -9,6 +9,8 @@
 #include <console.h>
 #include <kdebug.h>
 
+#include <string.h>	//add in lab1
+
 #define TICK_NUM 100
 
 static void print_ticks() {
@@ -46,6 +48,16 @@ idt_init(void) {
       *     You don't know the meaning of this instruction? just google it! and check the libs/x86.h to know more.
       *     Notice: the argument of lidt is idt_pd. try to find it!
       */
+	//add in lab1
+	extern uintptr_t __vectors[];
+    int i;
+    for (i = 0; i < sizeof(idt) / sizeof(struct gatedesc); i ++) {
+        SETGATE(idt[i], 0, GD_KTEXT, __vectors[i], DPL_KERNEL);
+    }
+	// set for switch from user to kernel
+    SETGATE(idt[T_SWITCH_TOK], 0, GD_KTEXT, __vectors[T_SWITCH_TOK], DPL_USER);
+	// load the IDT
+    lidt(&idt_pd);
 }
 
 static const char *
@@ -147,6 +159,11 @@ trap_dispatch(struct trapframe *tf) {
          * (2) Every TICK_NUM cycle, you can print some info using a funciton, such as print_ticks().
          * (3) Too Simple? Yes, I think so!
          */
+        ticks ++;
+        if (ticks % TICK_NUM == 0) {//TICK_NUM = 100
+            print_ticks();
+        }
+		//add in lab1
         break;
     case IRQ_OFFSET + IRQ_COM1:
         c = cons_getc();
